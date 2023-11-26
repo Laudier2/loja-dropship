e14747c150cf9572498ca81975b2f92c383de07b
Laudier2 <trabalhandooline2019@hotmail.com>
2023-11-25 23:45:11 -0300

    Update

--- src/components/products/Products2.jsx
+++ src/components/products/Products2.jsx
@@ -2,7 +2,7 @@
 //import { useHistory } from 'react-router-dom'
 import ReactLoading from 'react-loading';
 import { useSelector } from 'react-redux';
-import { ProductProd, LoadingPage } from './stylend';
+import { ProductProd, LoadingPage } from './stylend2';
 import { FaCreditCard } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 //import Slids from '../slids/slids';
@@ -85,7 +85,7 @@ const Products = () => {
               <h5>{res.name}</h5>
               <h3>R${res.price},00</h3>
               <span>
-                <FaCreditCard className='mt-1 m-1'/> Em até 12x sem juros
+                <FaCreditCard style={{marginTop: "0.7px", margin: 3}}/> Em até 12x sem juros
               </span>
               <button>
                 DESCRIÇÃO
---
+++ src/components/products/stylend2.js
@@ -0,0 +1,134 @@
+import styled from "styled-components";
+
+export const ProductProd = styled.div`
+  width: 100%;
+  background: transparent;
+  display: inline-block;
+
+  & section {
+    width: 100%;
+
+    & div {
+    width: 300px;
+    height: 300px;
+    display: inline-block;
+    margin-left: 10px;
+    margin-top: 25px;
+
+    & img {
+      width: 100%;
+      height: 40vh;
+      border-radius: 8px;
+      margin-bottom: 5px;
+      
+    }
+
+    & h5 {
+      width: 100%;
+      color: #000000;
+      font-weight: bold;
+      font-size: 13px;
+    }
+
+    & h3 {
+      color: #000000;
+      font-weight: bold;
+      text-align: center;
+    }
+
+    & span {
+      width: 60%;
+      margin: auto;
+      display: flex;
+      text-align: center;
+      color: rgba(10, 10, 10, 0.687);
+    }
+
+    & button {
+      background-color: aqua; /*#00FF00*/
+      width: 90%;
+      margin: auto;
+      display: block;
+      padding: 8px;
+      border-radius: 6px;
+      border: none;
+      font-size: 16px;
+      font-weight: bolder;
+      
+      &:hover {
+        background-color: #00FF00;
+      }
+
+    }
+    @media (max-width: 768px) {
+      width: 30%;
+      
+      & img {
+        width: 100%;
+        height: 25vh;
+        border-radius: 8px;
+        margin-bottom: 2px;
+
+        
+      }
+
+      & h5 {
+        width: 100%;
+        color: #000000;
+        font-weight: bold;
+        font-size: 1.8vw;
+        position: absolute;
+        margin-top: 5px;
+        width: 30%;
+      }
+
+      & h3 {
+        margin-top: 50px;
+        color: #000000;
+        font-weight: bold;
+        text-align: center;
+        font-size: 5vw;
+      }
+
+      & span {
+        width: 80%;
+        margin: auto;
+        display: flex;
+        text-align: center;
+        color: rgba(10, 10, 10, 0.687);
+        font-size: 2vw;
+      }
+
+      & button {
+        background-color: aqua; /*#00FF00*/
+        width: 100%;
+        margin: auto;
+        display: block;
+        padding: 8px;
+        border-radius: 6px;
+        border: none;
+        font-size:3vw;
+        font-weight: bolder;
+        
+        &:hover {
+          background-color: #00FF00;
+        }
+        }
+      }
+    }
+  }
+`
+export const LoadingPage1 = styled.div`
+ position: absolute;
+  top: 30%;
+  left: 45%;
+`
+export const LoadingPage = styled.div`
+  width: 20%;
+  margin: auto;
+  margin-top: 10%;
+  margin-bottom: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
